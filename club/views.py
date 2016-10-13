66a8a2797610f0d4260c646857d78a62d44dcbbf
Jake Shankman <jes3cu@virginia.edu>
2016-10-12 21:29:29 -0400

    Render basic page

--- club/views.py
+++ club/views.py
@@ -1,5 +1,11 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, logout
+from django.core.exceptions import PermissionDenied
+from django.core.mail import send_mass_mail
+from django.db.models import Q
 
 def index(request):
-	return HttpResponse("Hello, world!")
+	return render(request, "index.html")
