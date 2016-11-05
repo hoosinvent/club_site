0968caa16c485d2c6095582e7ed16a89a9017177
Jake Shankman <jes3cu@virginia.edu>
2016-11-05 13:58:15 -0400

    CRISPY

--- club/forms.py
+++ club/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit, Layout, Field
+from crispy_forms.bootstrap import FormActions
 
 class ContactForm(forms.Form):
     contact_name = forms.CharField(required=True)
@@ -8,3 +11,16 @@ class ContactForm(forms.Form):
         required=True,
         widget=forms.Textarea
     )
+    def __init__(self, *args, **kwargs):
+        super(ContactForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_class = 'form-horizontal'
+        self.helper.label_class = 'col-sm-2'
+        self.helper.field_class = 'col-sm-4'
+        self.helper.layout = Layout(
+            Field('contact_name', css_class='form-control'),
+            Field('contact_email', css_class='form-control'),
+            Field('email_subject', css_class='form-control'),
+            Field('content', css_class='form-control'),
+            FormActions(Submit('submit', 'Submit', css_class='btn btn-primary'))
+        )
