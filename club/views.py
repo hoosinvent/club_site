b01dc856456d26e8fc9d325dd71541d3f9f879f8
Jake Shankman <jes3cu@virginia.edu>
2016-11-05 13:02:38 -0400

    New email logic

--- club/views.py
+++ club/views.py
@@ -7,6 +7,7 @@ def index(request):
 	return render(request, "index.html")
 
 def contact(request):
+	response = None
 	if request.method == "GET":
 		form = ContactForm()
 	else:
@@ -14,13 +15,14 @@ def contact(request):
 		if form.is_valid():
 			contact_name = form.cleaned_data['contact_name']
 			contact_email = form.cleaned_data['contact_email']
+			email_subject = form.cleaned_data['email_subject']
 			content = form.cleaned_data['content']
 
 			sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
 			data = {
 				"personalizations": [{
 						"to": [{"email": "jes3cu@virginia.edu"}],
-						"subject": "Hello World from the SendGrid Python Library!"
+						"subject": email_subject
 					}],
 				"from": {"email": contact_email},
 				"content": [{
@@ -29,4 +31,5 @@ def contact(request):
 				}]
 			}
 			response = sg.client.mail.send.post(request_body=data)
-	return render(request, "contact.html", {'form':form})
+			form = ContactForm()
+	return render(request, "contact.html", {'form':form, 'email_sent':response})
