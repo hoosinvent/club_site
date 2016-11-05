3acb28cdc5d169389f8c82dd33ab70810ec7bbb9
Jake Shankman <jes3cu@virginia.edu>
2016-11-05 12:50:57 -0400

    Added email logic

--- club/views.py
+++ club/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .forms import *
 import sendgrid
 import os
 
@@ -7,20 +7,26 @@ def index(request):
 	return render(request, "index.html")
 
 def contact(request):
-	sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
-	data = {
-		"personalizations": [{
-				"to": [{"email": "jes3cu@virginia.edu"}],
-				"subject": "Hello World from the SendGrid Python Library!"
-			}],
-		"from": {"email": "jes3cu@virginia.edu"},
-		"content": [{
-				"type": "text/plain",
-				"value": "Hello, Email!"
-		}]
-	}
-	response = sg.client.mail.send.post(request_body=data)
-	print(response.status_code)
-	print(response.body)
-	print(response.headers)
-	return render(request, "contact.html")
+	if request.method == "GET":
+		form = ContactForm()
+	else:
+		form = ContactForm(data=request.POST)
+		if form.is_valid():
+			contact_name = form.cleand_data['contact_name']
+			contact_email = form.cleand_data['contact_email']
+			content = form.cleaned_data['content']
+
+			sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
+			data = {
+				"personalizations": [{
+						"to": [{"email": "jes3cu@virginia.edu"}],
+						"subject": "Hello World from the SendGrid Python Library!"
+					}],
+				"from": {"email": contact_email},
+				"content": [{
+						"type": "text/plain",
+						"value": content
+				}]
+			}
+			response = sg.client.mail.send.post(request_body=data)
+	return render(request, "contact.html", {'form':form})
